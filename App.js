8716763596727cd0e0d4e89d535a6027a434e0c5
sunanda <sunanda4549@gmail.com>
2025-07-10 09:38:21 +0530

    epidode3-part1

--- App.js
+++ App.js
@@ -1,32 +1,11 @@
-//creating h1 tag
-const heading = React.createElement(
-  "h1",
-  {
-    id: "heading",
-  },
-  "Hello World from React"
-);
-//creating root element to render div with id="root"
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+//create Element -this is basically an object when we render to DOM then it becomes html html element.
+// who is parsing this code? PARCEL is a manager,this transpiling is done by BABEL
+const heading = React.createElement("h1", { id: "heading" }, "Namaste React");
+
+//jsx syntax
+const jsxheading = <h1>Namaste React using Jsx</h1>;
 const root = ReactDOM.createRoot(document.getElementById("root"));
-//attaching heading to the root
 root.render(heading);
-
-/*
-<div id="parent"> 
-<div id="child">
-<h1>iam a h1 tag</h1>
-</div>
-</div>
-/*
-
-const parent = React.createElement(
-"div",{id:"parent"},
-React.createElement(
-"div",{id:"child"},
-React.createElement(
-"h1",{},"Iam a h1 tag"
-
-)
-)
-*/
-// );
